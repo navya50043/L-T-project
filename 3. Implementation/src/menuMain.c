5a1002dc0cf324ba08755e388b8ddaf5d4f1f875
navya50043 <66767118+navya50043@users.noreply.github.com>
2021-07-12 13:50:44 +0530

    Create menuMain.c

---
+++ 3. Implementation/src/menuMain.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<fun.h>
+
+
+void mainMenu() {
+
+    printf("******************Hello!*******************\n");
+    printf("**********Welcome to ATM Banking***********\n\n");
+    printf("****Please choose one of the options below****\n\n");
+    printf("< 1 >  Check Balance\n");
+    printf("< 2 >  Deposit\n");
+    printf("< 3 >  Withdraw\n");
+    printf("< 4 >  Exit\n\n");
+
+}
