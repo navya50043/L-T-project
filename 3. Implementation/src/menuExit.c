ad3b1ad2f5faadd214b35af9d212edd03002b67e
navya50043 <66767118+navya50043@users.noreply.github.com>
2021-07-12 14:15:50 +0530

    Update menuExit.c

--- 3. Implementation/src/menuExit.c
+++ 3. Implementation/src/menuExit.c
@@ -1,6 +1,6 @@
 
 #include<stdio.h>
-#include<fun.h>
+#include"fun.h"
 void menuExit() {
     printf("--------------Take your receipt!!!------------------\n");
     printf("-----Thank you for using ATM Banking Machine!!!-----\n");
