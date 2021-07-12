d1738d9653686c9e74da2c7791907eb7e9706433
navya50043 <66767118+navya50043@users.noreply.github.com>
2021-07-12 14:13:43 +0530

    Update checkBalance.c

--- 3. Implementation/src/checkBalance.c
+++ 3. Implementation/src/checkBalance.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<fun.h>
+#include"fun.h"
 void checkBalance(float balance) {
     printf("You Choose to See your Balance\n");
     printf("\n\n****Your Available Balance is:   $%.2f\n\n", balance);
