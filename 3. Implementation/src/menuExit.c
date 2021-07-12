421ca057d89ebe1edb602ac29e44ed9d0fa8e5ee
navya50043 <66767118+navya50043@users.noreply.github.com>
2021-07-12 14:04:41 +0530

    Add files via upload

---
+++ 3. Implementation/src/menuExit.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+#include<fun.h>
+void menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
+
+
+
+}
