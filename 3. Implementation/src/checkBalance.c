25bc424cca34a3cd4f5694bc1d47d3c49bdb039b
navya50043 <66767118+navya50043@users.noreply.github.com>
2021-07-12 13:54:38 +0530

    Add files via upload

---
+++ 3. Implementation/src/checkBalance.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<fun.h>
+void checkBalance(float balance) {
+    printf("You Choose to See your Balance\n");
+    printf("\n\n****Your Available Balance is:   $%.2f\n\n", balance);
+
+}
